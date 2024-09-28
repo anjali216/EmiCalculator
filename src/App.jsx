51460334772f6c83a6f2558a4486b2f3338b94c6
Your Name <anjalik216@gmail.com>
2024-09-28 20:06:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Emi from './Components/Emi';
+import Header from './Components/Header';
+
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+   <>
+   
+   <Header/>
+   <Emi/>
+   <Footer/>
+   
+   
+   </> 
+      
+  )
+}
+
+export default App;
